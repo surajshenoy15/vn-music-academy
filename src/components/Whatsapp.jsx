6392ea092c24bbcbfed276b8b80808e5accd61bc
suraj shenoy <surajshenoyp@gmail.com>
2025-09-14 18:32:53 +0530

    added whatsapp number

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const WhatsAppButton = () => {
-  const whatsappUrl = "https://wa.link/ohakyj"; // ✅ Your custom wa.link
+  const whatsappUrl = "https://wa.link/ri4htz"; // ✅ Your custom wa.link
 
   return (
     <div className="fixed bottom-6 right-6 z-50">
